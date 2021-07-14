c333c6521522659bd5a8e08fb4fbbc44ff932976
geoffrey <liujingshuai20@126.com>
2021-07-14 16:29:24 +0800

    git首次提交

---
+++ modules/GitTest/src/main/java/com/geoffrey/gittest/GitTestActivity.java
@@ -0,0 +1,26 @@
+package com.geoffrey.gittest;
+
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * -------------------------------------
+ * 作者：刘京帅 sumu
+ * -------------------------------------
+ * 时间：7/14/21
+ * -------------------------------------
+ * 描述：
+ * -------------------------------------
+ * 备注：
+ * -------------------------------------
+ */
+public class GitTestActivity extends AppCompatActivity {
+
+    private final String TAG = "GitTestActivity";
+    private void gitTest(){
+
+        Log.e(TAG,"gittest shou首次提交");
+    }
+
+}
